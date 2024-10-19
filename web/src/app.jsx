d1e81d1cc123b5fe8370db55b3d47693ef74d79c
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2024-10-19 12:07:46 +0700

    feat: Setup layout, color + UI sidebar

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppRoutes from './routes/AppRoutes.jsx'
+import './assets/css/global.css'
 import { StateProvider } from './context/StateContext.jsx'
 import reducer, { initialState } from './context/StateReducers.jsx'
+import AppRoutes from './routes/AppRoutes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ web/src/constants/Colors.js
@@ -0,0 +1,26 @@
+const colors = {
+    primary: '#4A75F7',
+    secondary: '#65A9FB',
+    success: '#52c41a',
+    danger: '#f5222d',
+    warning: '#faad14',
+    info: '#13c2c2',
+    light: '#f0f2f5',
+
+    muted: '#F3E7E9',
+    background: '#F3F3FD',
+    darkBackground: '#303030',
+    textPrimary: '#303030',
+    textSecondary: '#969AA0',
+    border: '#d9d9d9',
+    highlight: '#FFCA42',
+    shadow: 'rgba(0, 0, 0, 0.1)',
+    gradientStart: '#E3EEFF',
+    gradientEnd: '#F3E7E9',
+
+    grayLight: '#f5f5f5',
+    grayMedium: '#bfbfbf',
+    grayDark: '#404040',
+};
+
+export default colors;
