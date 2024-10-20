39b812f06449420cbc7f8ab1503b45bf5b690982
Thanh Vinh <ptvinh203@gmail.com>
2024-10-20 19:10:37 +0700

    UI: customer order page

--- server/src/main/java/com/dut/healthme/controller/ItemController.java
+++ server/src/main/java/com/dut/healthme/controller/ItemController.java
@@ -1,28 +1,26 @@
 package com.dut.healthme.controller;
 
 import com.dut.healthme.common.model.AbstractResponse;
-import com.dut.healthme.dto.request.LoginRequest;
 import com.dut.healthme.dto.response.ListRecommendResponse;
 import com.dut.healthme.service.ItemService;
 import io.lettuce.core.dynamic.annotation.Param;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/Item")
+@RequestMapping("/v1/item")
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+
     @GetMapping("/recommend")
     public ResponseEntity<AbstractResponse> GetRecommend2List(@Param("calo") double calo) {
         var listRecommend1 = this.itemService.getItemsByEvaluate();
         var listRecommend2 = this.itemService.getItemsByCaloRec(calo);
-        return ResponseEntity.ok(AbstractResponse.successWithoutMeta( new ListRecommendResponse(listRecommend1,listRecommend2)));
+        return ResponseEntity.ok(AbstractResponse.successWithoutMeta(new ListRecommendResponse(listRecommend1, listRecommend2)));
     }
 
 }
