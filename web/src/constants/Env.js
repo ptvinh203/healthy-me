7ae985fade333918178de60beb9fed14f8b5a74f
MINTIN17 <hmtintk@gmail.com>
2024-11-02 12:23:03 +0700

    link server

--- web/src/constants/Env.js
+++ web/src/constants/Env.js
@@ -1,5 +1,5 @@
 // All environment variables should be defined here
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1/';
 
 export { API_BASE_URL };
