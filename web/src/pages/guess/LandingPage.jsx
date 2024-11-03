55bdcdb5c6fe7f682eb5c339fb85a1a9d3ba0a50
main <mhung9911@gmail.com>
2024-11-03 13:06:26 +0700

    :D

--- web/src/pages/guess/LandingPage.jsx
+++ web/src/pages/guess/LandingPage.jsx
@@ -2,9 +2,7 @@ import Footer from "../../components/guest/Footer";
 import Header from "../../components/guest/Header";
 import FeatureSection from "../../components/guest/FeatureSection";
 import SuggestionSection from "../../components/guest/SuggestionSection";
-import SearchBar from "../../components/guest/SearchBar";
-import image from "../../assets/images/doctor.png";
-import { Col, Row } from "antd";
+import Image from "../../components/guest/Image";
 
 function LandingPage() {
     return (
@@ -21,22 +19,12 @@ function LandingPage() {
                     style={{
                         padding: "80px",
                         width: "100%",
+                        display: "flex",
+                        justifyContent: "flex-end"
                     }}
                 >
-                    <Row >
-                        <Col span={12}>
-                            <SearchBar />
-                        </Col>
-                        <Col
-                            span={12}
-                            style={{
-                                display: "flex",
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <img style={{ width: "60%" }} src={image}></img>
-                        </Col>
-                    </Row>
+                    {/* <SearchBar></SearchBar> */}
+                    <Image></Image>
                 </div>
                 <FeatureSection />
                 <SuggestionSection />
