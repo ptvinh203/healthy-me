1b1b79b7152a34b88aa820348126083e935cac09
main <mhung9911@gmail.com>
2024-11-03 13:05:13 +0700

    :D

--- web/src/components/guest/SearchBar.jsx
+++ web/src/components/guest/SearchBar.jsx
@@ -1,15 +1,15 @@
-import { Input, Button, Flex } from "antd";
+import { Input, Button, Flex, Typography } from "antd";
 import { HeartTwoTone, SearchOutlined, CloseCircleFilled } from "@ant-design/icons";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+import colors from "../../constants/Colors";
 const CustomSearchBar = () => {
     const suffix = <HeartTwoTone twoToneColor="#eb2f96" />;
     const [query, setQuery] = useState("");
     const [active, setActive] = useState(false);
-    const testApi = "https://jsonplaceholder.typicode.com/albums";
+    const testApi = "https://jsonplaceholder.typicode.com/photos";
     const [searchResults, setSearchResults] = useState([]);
-    console.log(searchResults);
+    const { Text, Paragraph } = Typography;
+
     useEffect(() => {
         const controller = new AbortController();
         async function fetchData() {
@@ -25,7 +25,7 @@ const CustomSearchBar = () => {
                     })
                 );
             } catch (error) {
-                if (error.name != "AbortError") console.log(error.message);
+                if (error.name !== "AbortError") console.log(error.message);
             }
         }
 
@@ -35,49 +35,90 @@ const CustomSearchBar = () => {
             setSearchResults([]);
         }
 
-        fetchData();
         return function () {
             controller.abort();
         };
     }, [query]);
 
-    useEffect(() => {
-        query.length > 0 ? setActive(true) : setActive(false);
-    }, [query]);
+    function focus() {
+        setActive(true);
+    }
 
+    function blur() {
+        setActive(false);
+    }
+
+    function clearQuery() {
+        setQuery("");
+    }
+    console.log(searchResults);
     return (
         <div style={{ display: "flex", alignItems: "flex-start", width: "500px" }}>
             <Flex
+                onFocus={focus}
+                onBlur={blur}
                 style={{
+                    marginRight: "10px",
                     width: active ? "100%" : "60%",
                     transition: "width 0.3s ease, transform 0.3s ease",
                     transform: active ? "scale(1.05)" : "scale(1)",
+                    position: "relative",
                 }}
                 vertical
             >
                 <Input
                     placeholder="Vấn đề sức khỏe"
                     suffix={suffix}
-                    onChange={(e) => {
-                        setQuery(e.target.value);
-                    }}
+                    onChange={(e) => setQuery(e.target.value)}
                     value={query}
                     style={{
                         width: "100%",
                         borderRadius: "20px",
                         padding: "5px 10px",
-                        borderColor: "#d9d9d9",
+                        borderColor: colors.border,
                     }}
                 />
-
-                {query.length > 0 ? (
-                    <Flex vertical></Flex>
-                ) : (
-                    <div style={{ width: "100%" }}>
-                        <span>Hãy nhập tên nhà hàng bạn cần tìm hjhj</span>
-                    </div>
+                {query.length >= 3 && (
+                    <Flex
+                        vertical
+                        style={{
+                            width: "100%",
+                            height: "300px",
+                            position: "absolute",
+                            top: "38px",
+                            border: `solid 1px ${colors.border}`,
+                            overflowY: "scroll",
+                            scrollbarWidth: "thin",
+                            scrollbarColor: colors.grayMedium,
+                            overflowX: "hidden",
+                        }}
+                    >
+                        <Paragraph strong style={{ width: "100%", margin: "10px 0px 0px 10px" }}>
+                            <Text style={{ fontSize: "large" }}>Restaurant</Text>
+                        </Paragraph>
+                        {searchResults.length == 0 ? (
+                            <Paragraph strong style={{ width: "100%", margin: "10px 0px 0px 10px" }}>
+                                <Text style={{ fontSize: "medium" }}>Không tìm thấy nhà hàng</Text>
+                            </Paragraph>
+                        ) : (
+                            searchResults.map((item) => (
+                                <Flex
+                                    key={item.id}
+                                    style={{
+                                        padding: "10px",
+                                        borderTop: `solid 1px ${colors.border}`,
+                                    }}
+                                    vertical
+                                >
+                                    <div>{item.title}</div>
+                                    <div>{item.id}</div>
+                                </Flex>
+                            ))
+                        )}
+                    </Flex>
                 )}
             </Flex>
+
             {/* route res */}
             {query.length > 0 ? (
                 <Button
@@ -85,11 +126,12 @@ const CustomSearchBar = () => {
                     shape="circle"
                     icon={<CloseCircleFilled />}
                     style={{
-                        marginLeft: "10px",
+                        cursor: "pointer",
                     }}
-                    onClick={() => {
-                        setQuery("");
+                    onMouseDown={(event) => {
+                        event.preventDefault(); // Prevent input blur
                     }}
+                    onClick={clearQuery} // Call clearQuery directly
                 />
             ) : (
                 <Button
@@ -97,6 +139,7 @@ const CustomSearchBar = () => {
                     shape="circle"
                     icon={<SearchOutlined />}
                     style={{
+                        cursor: query.length > 0 ? "pointer" : "auto",
                         marginLeft: "10px",
                     }}
                 />
